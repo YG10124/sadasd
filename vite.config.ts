88e75cf2ace10b4f2f92342880415279afb53094
unknown <1057225@lcps.org>
2026-03-02 14:16:25 -0500

    third-second commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,7 +11,7 @@ const __dirname = path.dirname(__filename);
 export default defineConfig({
   // Use a relative base so GitHub Pages works regardless of repo/project path.
   base: "./",
-  plugins: [react(), tailwindcss(), viteSingleFile()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
